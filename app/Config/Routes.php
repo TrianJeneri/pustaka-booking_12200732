103ead43f344015822a85bbd09696282aea619c5
TrianJeneri <mcdessert17@gmail.com>
2021-10-02 17:36:48 +0700

    Tugas Pertemuan 3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,6 +34,7 @@ $routes->setAutoRoute(true);
 $routes->get('/', 'Home::index');
 $routes->get('/hello', 'Selamatdatang::hal_awal');
 $routes->get('/login', 'Selamatdatang::beranda_login');
+$routes->post('/login', 'Login::cekLogin');
 $routes->get('/daftar-member', 'Selamatdatang::daftar_member');
 
 /*
